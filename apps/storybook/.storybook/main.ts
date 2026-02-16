2c1db55d27df9abdac163caf14fdec1cb9802625
Dobbymin <gmin990624@gmail.com>
2026-02-17 02:32:26 +0900

    feat: vanilla extract vite 설정 추가

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,6 +1,8 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { mergeConfig } from 'vite';
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -22,5 +24,10 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-docs'),
   ],
   framework: getAbsolutePath('@storybook/react-vite'),
+  viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [vanillaExtractPlugin()],
+    });
+  },
 };
 export default config;
